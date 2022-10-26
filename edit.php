1f150a9a3902d2beebcfd68f7462a045016c27f9
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-10-26 10:34:28 -0500

    Create edit.php

---
+++ edit.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>GFG- Delete Data</title>
+   </head>
+   <body>
+      <center>
+         <h1>Delete Form data in Database</h1>
+         <form action="delete2.php" method="post">
+             
+ 
+             
+<p>
+               <label for="ID1">Update a Customer with this ID:</label>
+               <input type="text" name="ID1" id="ID1">
+            </p>
+ <p>
+               <label for="firstName2">What do you wish to change this Customer's Name to?:</label>
+               <input type="text" name="first_name2" id="firstName2">
+            </p>
+             
+ 
+             
+<p>
+               <label for="manufacturerID">Update Manufacturers with this Manufacturer ID:</label>
+               <input type="text" name="manufacturerid2" id="manufacturerID2">
+            </p>
+ <p>
+               <label for="ManufacturerName2">What do you wish to change this Manufacturer's Name to?:</label>
+               <input type="text" name="manufacturername2" id="ManufacturerName2">
+            </p>
+             
+ <p>
+               <label for="productID"> Update Product Name with this Product ID:</label>
+               <input type="text" name="productid3" id="productID">
+            </p>      
+          <p>
+               <label for="productName">What do you wish to change the product name to?:</label>
+               <input type="text" name="productname2" id="productName2">
+            </p>
+              <p>
+               <label for="saleid"> Update Orders with this Sale ID:</label>
+               <input type="text" name="saleid1" id="saleid">
+                   <p>
+               <label for="saleprice">What is the new sale price for these orders?:</label>
+               <input type="text" name="saleprice2" id="saleprice2">
+            </p>  
+            <input type="submit" value="Submit">
+                           
+         </form>
+             </div>
+      </center>
+   </body>
+</html>
