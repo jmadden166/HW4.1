e34dbc2a17a3df30fa6399763f64ce99ec10adce
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-10-26 10:17:57 -0500

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>GFG- Delete Data</title>
+   </head>
+   <body>
+      <center>
+         <h1>Delete Form data in Database</h1>
+         <form action="delete2.php" method="post">
+             
+ 
+             
+<p>
+               <label for="firstName">Delete a Customer with this First Name:</label>
+               <input type="text" name="first_name" id="firstName">
+            </p>
+ 
+             
+ 
+             
+<p>
+               <label for="manufacturerID">Delete Orders with this Manufacturer ID:</label>
+               <input type="text" name="manufacturerid" id="manufacturerID">
+            </p>
+ 
+             
+ <p>
+               <label for="productID"> Delete Orders with this Product ID:</label>
+               <input type="text" name="productid" id="productID">
+            </p>      
+              <p>
+               <label for="saleid"> Delete Orders with this Sale ID:</label>
+               <input type="text" name="saleid" id="saleid">
+            <input type="submit" value="Submit">
+         </form>
+             </div>
+      </center>
+   </body>
+</html>
