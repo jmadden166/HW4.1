ef32fcfc5d5a9d960a7f994542b632a00e50f325
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-10-25 19:38:04 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>GFG- Store Data</title>
+   </head>
+   <body>
+      <center>
+         <h1>Storing Form data in Database</h1>
+         <form action="insert.php" method="post">
+             
+<p>
+               <label for="custID">Customer ID:</label>
+               <input type="text" name="custID" id="custID">
+            </p>
+ 
+             
+<p>
+               <label for="firstName">First Name:</label>
+               <input type="text" name="first_name" id="firstName">
+            </p>
+ 
+             
+<p>
+               <label for="ManufacturerName">Manufacturer Name:</label>
+               <input type="text" name="manufacturername" id="ManufacturerName">
+            </p>
+ 
+             
+<p>
+               <label for="manufacturerID">Manufacturer ID:</label>
+               <input type="text" name="manufacturerid" id="manufacturerID">
+            </p>
+ 
+             
+<p>
+               <label for="productName">Product Name:</label>
+               <input type="text" name="productname" id="productName">
+            </p>
+ <p>
+               <label for="productID">Product ID:</label>
+               <input type="text" name="productid" id="productID">
+            </p>
+ <p>
+               <label for="shipping">Shipping Fee:</label>
+               <input type="text" name="shipping" id="shipping">
+            </p>
+    <p>
+               <label for="tax">Tax Fee:</label>
+               <input type="text" name="tax" id="tax">
+            </p>        
+         <p>
+               <label for="quantity">Quantity:</label>
+               <input type="text" name="quantity" id="quantity">
+            </p>         
+              <p>
+               <label for="saleid">Sale ID:</label>
+               <input type="text" name="saleid" id="saleid">
+            </p>  
+              <p>
+               <label for="saleprice">Sale Price:</label>
+               <input type="text" name="saleprice" id="saleprice">
+            </p>  
+            <input type="submit" value="Submit">
+         </form>
+      </center>
+   </body>
+</html>
